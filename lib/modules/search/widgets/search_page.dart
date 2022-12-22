e4a4b9e9749f097e78fde897803f0d4eb8553c02
hyehy <94891488+hyehy@users.noreply.github.com>
2022-12-22 18:36:37 +0900

    검색어 발리데이트

--- lib/modules/search/widgets/search_page.dart
+++ lib/modules/search/widgets/search_page.dart
@@ -16,49 +16,60 @@ class SearchPage extends GetView<SearchController> {
         children: [
           Padding(
             padding: const EdgeInsets.all(20),
-            child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                  borderRadius: BorderRadius.circular(10.0)),
-              child: Form(
-                key: controller.formKey,
-                child: TextFormField(
-                  maxLength: 10,
-                  controller: controller.searchTxt,
-                  focusNode: controller.searchNode,
-                  decoration: InputDecoration(
-                      counterText: '',
-                      border: InputBorder.none,
-                      errorBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black12),
-                        borderRadius: BorderRadius.circular(20),
+            child: Form(
+              key: controller.formKey,
+              child: TextFormField(
+                maxLength: 10,
+                controller: controller.searchTxt,
+                focusNode: controller.searchNode,
+                decoration: InputDecoration(
+                    counterText: '',
+                    enabledBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Colors.black,
+                        )),
+                    focusedBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Colors.black,
+                        )),
+                    errorBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Colors.red,
+                        )),
+                    focusedErrorBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4)),
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: Colors.red,
                       ),
-                      focusedErrorBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black12),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      hintText: '검색어를 입력해주세요.'.tr,
-                      hintStyle: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
-                      ),
-                      contentPadding: const EdgeInsets.all(20.0),
-                      suffixIcon: const Icon(
-                        Icons.search,
-                        color: Colors.black,
-                        size: 30,
-                      )),
-                  validator: (val) {
-                    if (val!.length < 2) {
-                      return '검색어는 2자 이상 입력해주세요.';
-                    }
-                    return null;
-                  },
-                  onChanged: (text) {
-                    controller.searchEvent(text);
-                    print(text);
-                  },
-                ),
+                    ),
+                    hintText: '검색어를 입력해주세요.'.tr,
+                    hintStyle: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                    contentPadding: const EdgeInsets.all(20.0),
+                    suffixIcon: const Icon(
+                      Icons.search,
+                      color: Colors.black,
+                      size: 30,
+                    )),
+                validator: (val) {
+                  if (val!.length < 2) {
+                    return '검색어는 2자 이상 입력해주세요.';
+                  }
+                  return null;
+                },
+                onChanged: (text) {
+                  controller.searchEvent(text);
+                  print(text);
+                },
               ),
             ),
           ),
